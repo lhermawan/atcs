93b67e402f2cf85568ac503410392b1fa2fffacb
Lucky Hermawan Roza <luckyhermawanroza@gmail.com>
2026-09-18 12:58:24 +0700

    feat: add ATCS logo as favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ATCS Ciamis - Live Monitoring</title>
+    <link rel="icon" href="{{ asset('img/logo.jpg') }}" type="image/jpeg">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
